de5e3acfb94a2d184d2539c3f9f053d31d18be4c
zedroopy <ze_droopy@yahoo.fr>
2020-04-10 15:34:54 +0200

    Initial commit

---
+++ DCS_Radio_Mission_Mod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+namespace DCS_Radio_Mission_Mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (string X in Y)
+            {
+                Console.WriteLine("");
+
+                // F-14
+
+                string file =  "mission";
+                string search = "";
+                string replace = "";
+                File.WriteAllText(file, File.ReadAllText(file).Replace(search, replace));
+                
+            }
+
+        }
+    }
+}
